052314cb59d815491caffa06959572d4134e62fd
Drew Jowers <drewski3420@gmail.com>
2018-02-07 18:39:38 -0500

    changed the log location

--- logger.py
+++ logger.py
@@ -1,13 +1,14 @@
 import logging
 import os
+from os.path import expanduser
 
 def setup_custom_logger(name):
     #logging.getLogger('urllib3').setLevel(logging.WARNING)
     #logging.getLogger('requests').setLevel(logging.WARNING)
     #logging.getLogger('chardet').setLevel(logging.WARNING)
     
-    dir = os.path.dirname(__file__)
-    fn = os.path.join(dir, '~/sunray.log')
+    dir = expanduser('~')
+    fn = os.path.join(dir, 'sunray.log')
     formatter = logging.Formatter(fmt='%(asctime)s | %(name)s | %(levelname)-8s | %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
     handler = logging.FileHandler(fn, mode='a',encoding = 'UTF-8')
