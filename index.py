10bbec6c0f9cd03a994850bfb551032b0d1b8ea1
Drew Jowers <drewski3420@gmail.com>
2018-02-07 18:19:15 -0500

    new logging

--- index.py
+++ index.py
@@ -15,10 +15,10 @@ from dateutil import parser
 import pytz
 import logger as l
 try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+	import argparse
+	flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
-    flags = None
+	flags = None
 
 logger = l.setup_custom_logger(__name__)
 
@@ -27,106 +27,126 @@ CLIENT_SECRET_FILE = 'configs/client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
 def get_credentials():
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir,'.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,'calendar-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+	try:
+		home_dir = os.path.expanduser('~')
+		credential_dir = os.path.join(home_dir,'.credentials')
+		if not os.path.exists(credential_dir):
+			os.makedirs(credential_dir)
+		credential_path = os.path.join(credential_dir,'calendar-python-quickstart.json')
+	
+		store = Storage(credential_path)
+		credentials = store.get()
+		if not credentials or credentials.invalid:
+			flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+			flow.user_agent = APPLICATION_NAME
+			if flags:
+				credentials = tools.run_flow(flow, store, flags)
+			else: # Needed only for compatibility with Python 2.6
+				credentials = tools.run(flow, store)
+			print('Storing credentials to ' + credential_path)
+	except Exception as e:
+		logging.exception('Error in get_credentials()',exc_info=True)
+	return credentials
 
 def build_event(name,run_time,start_date,end_date,plot,id,year):
-    event = {}
-    event['start'] = {}
-    event['end'] = {}
-    event['attachments'] = {}
-    event['attendees'] = {}
-    start_str = start_date.strftime('%Y-%m-%dT%H:%M:%S')
-    end_str = end_date.strftime('%Y-%m-%dT%H:%M:%S')
-    event['summary'] = name
-    event['location'] = '1028 Park St, Jacksonville, FL 32204'
-    description = ''
-    if id:
-        description += 'http://www.imdb.com/title/{}/'.format(id)
-    if year:
-        description += '\nYear: {}'.format(year)
-    if plot and plot != 'N/A':
-		description += '\nPlot: {}'.format(plot)
-    if run_time:
-		description += '\nRun Time: {}'.format(run_time)
-    event['description'] = description
-    event['start']['dateTime'] = start_str
-    event['start']['timeZone'] = 'America/New_York'
-    event['end']['dateTime'] = end_str
-    event['end']['timeZone'] = 'America/New_York'
-    return event
+	try:
+		event = {}
+		event['start'] = {}
+		event['end'] = {}
+		event['attachments'] = {}
+		event['attendees'] = {}
+		start_str = start_date.strftime('%Y-%m-%dT%H:%M:%S')
+		end_str = end_date.strftime('%Y-%m-%dT%H:%M:%S')
+		event['summary'] = name
+		event['location'] = '1028 Park St, Jacksonville, FL 32204'
+		description = ''
+		if id:
+			description += 'http://www.imdb.com/title/{}/'.format(id)
+		if year:
+			description += '\nYear: {}'.format(year)
+		if plot and plot != 'N/A':
+			description += '\nPlot: {}'.format(plot)
+		if run_time:
+			description += '\nRun Time: {}'.format(run_time)
+		event['description'] = description
+		event['start']['dateTime'] = start_str
+		event['start']['timeZone'] = 'America/New_York'
+		event['end']['dateTime'] = end_str
+		event['end']['timeZone'] = 'America/New_York'
+	except Exception as e:
+		logging.exception('Error in build_event({}, {})'.format(name,start_date),exc_info=True)
+	return event
 
 def get_cal_id(service):
-    cals = service.calendarList().list().execute()
-    for cal in cals['items']:
-        if cal['summary'] == 'Sun-Ray Cinema':
-            cal_id = cal['id']
-    return cal_id
-    
+	try:
+		cals = service.calendarList().list().execute()
+		for cal in cals['items']:
+			if cal['summary'] == 'Sun-Ray Cinema':
+				cal_id = cal['id']
+	except Exception as e:
+		logging.exception('Error in get_cal_id()',exc_info=True)
+	return cal_id
+	
 def clear_calendar(cal_id,service):
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time   
-    eventsResult = service.events().list(calendarId=cal_id, timeMin=now, singleEvents=True, orderBy='startTime').execute()
-    for event in eventsResult['items']:
-        service.events().delete(calendarId=cal_id, eventId=event['id']).execute()
+	try:
+		now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time   
+		eventsResult = service.events().list(calendarId=cal_id, timeMin=now, singleEvents=True, orderBy='startTime').execute()
+		for event in eventsResult['items']:
+			service.events().delete(calendarId=cal_id, eventId=event['id']).execute()
+	except Exception as e:
+		logging.exception('Error in clear_calendar({})'.format(cal_id),exc_info=True)
 
 def add_event(service, ev, cal_id):
-    service.events().insert(calendarId=cal_id, body=ev).execute()
-    
+	try:
+		service.events().insert(calendarId=cal_id, body=ev).execute()
+	except Exception as e:
+		logging.exception('Error in add_event()',exc_info=True)
+	
 def main():
-    credentials = get_credentials()
-    logger.info('Got Credentials')
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
-    logger.info('Got Service')
-    #get calendar ID
-    cal_id = get_cal_id(service)
-    logger.info('Got Cal_id: {}'.format(cal_id))
-    #first delete all upcoming events
-    clear_calendar(cal_id,service)
-    logger.info('Cleared Calendar')
-    #now add new events - first get list of current SunRay showtimes
-    movies = sunray.main()
-    logger.info('Got Movies from sunray.py')
-    logger.info('Number of Movies (showtimes) received: {}'.format(len(movies)))
-    #now loop through and add event
-    for movie in movies:
-        logger.info('Processing movie {}, Date {}, Time {}'.format(movie['name'], movie['date'], movie['show_time']))
-        name = movie['name']
-        run_time = parser.parse(movie['run_time']).time()
-        show_start_date = parser.parse(movie['date'])
-        show_start_time = parser.parse(movie['show_time']).time()
-        plot = movie['plot']
-        id = movie['id']
-        year = movie['year']
-        show_start_date_time = datetime.datetime.combine(show_start_date,show_start_time)
-        show_end_date_time = show_start_date_time + timedelta(hours=run_time.hour,minutes=run_time.minute)
-        show_start_with_tz = show_start_date_time
-        show_end_with_tz = show_end_date_time
-        #only add if it's after work on a weekday (keeps  calendar cleaner)
-        if (show_start_time >= parser.parse('17:00:00').time()) or (show_start_date_time.strftime('%A') in ('Saturday','Sunday')):
-            logger.info('Show time qualifies to be added')
-            ev = build_event(name,run_time,show_start_with_tz,show_end_with_tz,plot,id,year)
-            logger.info('Event built')
-            add_event(service,ev,cal_id)
-            logger.info('Event added')
+	try:
+		credentials = get_credentials()
+		logger.info('Got Credentials')
+		http = credentials.authorize(httplib2.Http())
+		service = discovery.build('calendar', 'v3', http=http)
+		logger.info('Got Service')
+		#get calendar ID
+		cal_id = get_cal_id(service)
+		logger.info('Got Cal_id: {}'.format(cal_id))
+		#first delete all upcoming events
+		clear_calendar(cal_id,service)
+		logger.info('Cleared Calendar')
+		#now add new events - first get list of current SunRay showtimes
+		movies = sunray.main()
+		logger.info('Got Movies from sunray.py')
+		logger.info('Number of Movies (showtimes) received: {}'.format(len(movies)))
+		#now loop through and add event
+		for movie in movies:
+			logger.info('Processing movie {}, Date {}, Time {}'.format(movie['name'], movie['date'], movie['show_time']))
+			name = movie['name']
+			run_time = parser.parse(movie['run_time']).time()
+			show_start_date = parser.parse(movie['date'])
+			show_start_time = parser.parse(movie['show_time']).time()
+			plot = movie['plot']
+			id = movie['id']
+			year = movie['year']
+			show_start_date_time = datetime.datetime.combine(show_start_date,show_start_time)
+			show_end_date_time = show_start_date_time + timedelta(hours=run_time.hour,minutes=run_time.minute)
+			show_start_with_tz = show_start_date_time
+			show_end_with_tz = show_end_date_time
+			#only add if it's after work on a weekday (keeps  calendar cleaner)
+			if (show_start_time >= parser.parse('17:00:00').time()) or (show_start_date_time.strftime('%A') in ('Saturday','Sunday')):
+				logger.info('Show time qualifies to be added')
+				ev = build_event(name,run_time,show_start_with_tz,show_end_with_tz,plot,id,year)
+				logger.info('Event built')
+				add_event(service,ev,cal_id)
+				logger.info('Event added')
+	except Exception as e:
+		logging.exception('Error in main()',exc_info=True)
 
 if __name__ == '__main__':
-    main()
+	logger.info('-----------------------------------Start of Script---------------------------------------')
+	main()
+	logger.info('-----------------------------------End of Script---------------------------------------')
 
 def lambda_handler(event, context):
-    main()
+	main()
--- sunray.py
+++ sunray.py
@@ -10,178 +10,202 @@ logger = l.setup_custom_logger(__name__)
 ENDPOINT = 'http://14438.formovietickets.com:2235/T.ASP'
 
 def get_dates():
-	dates = []
-	params = {'SelectedDate' : d.now().strftime('%Y%m%d')
-				,'Page' : 'schedule'
-				,'WCI' : 'BT'}
-	r = requests.get(ENDPOINT,params=params, headers={'Connection':'close'})
-	soup = BeautifulSoup(r.text,'html5lib')
-	form = soup.body.table.tbody.tr.form
-	for option in form.find_all('option'):
-		date = d.strptime(option.text,'%a %b %d, %Y') #get date
-		dates.append(date)
-	logger.info('Got Dates')
-	logger.info('Number of dates found: {}'.format(len(dates)))
+	try:
+		dates = []
+		params = {'SelectedDate' : d.now().strftime('%Y%m%d')
+					,'Page' : 'schedule'
+					,'WCI' : 'BT'}
+		r = requests.get(ENDPOINT,params=params, headers={'Connection':'close'})
+		soup = BeautifulSoup(r.text,'html5lib')
+		form = soup.body.table.tbody.tr.form
+		for option in form.find_all('option'):
+			date = d.strptime(option.text,'%a %b %d, %Y') #get date
+			dates.append(date)
+		logger.info('Got Dates')
+		logger.info('Number of dates found: {}'.format(len(dates)))
+	except Exception as e:
+		logging.exception('Error in get_dates()',exc_info=True)
 	return dates
 
 def get_daily_html(date):
-	params = {'SelectedDate' : date.strftime('%Y%m%d') #add params
-						,'Page' : 'schedule'
-						,'WCI' : 'BT'}
-	r = requests.get(ENDPOINT,params=params, headers={'Connection':'close'})
-	soup = BeautifulSoup(r.text,'html5lib')
-	strings = [text for text in soup.stripped_strings]
-	logger.info('Got Daily Html Strings')
+	try:
+		params = {'SelectedDate' : date.strftime('%Y%m%d') #add params
+							,'Page' : 'schedule'
+							,'WCI' : 'BT'}
+		r = requests.get(ENDPOINT,params=params, headers={'Connection':'close'})
+		soup = BeautifulSoup(r.text,'html5lib')
+		strings = [text for text in soup.stripped_strings]
+		logger.info('Got Daily Html Strings for date {}'.format(date))
+	except Exception as e:
+		logging.exception('Error in get_daily_html({})'.format(date),exc_info=True)
 	return strings
 
 def get_movie_details_from_html(strings):
-	for idx, string in enumerate(strings): #find the first date, so we can begin parsing in the next block
-		try:
-			d.strptime(string,'%a %b %d, %Y')
-			first_date = idx
-			break
-		except Exception as ex:
-			pass
-	for idx, string in enumerate(strings[first_date:],first_date): #starting at the first date above, go until we can't parse anymore. that's where the movies will start
-		try:
-			d.strptime(string,'%a %b %d, %Y')
-		except Exception as ex:
-			pass
-			movie_start = idx
-			break
+	try:
+		for idx, string in enumerate(strings): #find the first date, so we can begin parsing in the next block
+			try:
+				d.strptime(string,'%a %b %d, %Y')
+				first_date = idx
+				break
+			except Exception as ex:
+				pass
+		for idx, string in enumerate(strings[first_date:],first_date): #starting at the first date above, go until we can't parse anymore. that's where the movies will start
+			try:
+				d.strptime(string,'%a %b %d, %Y')
+			except Exception as ex:
+				pass
+				movie_start = idx
+				break
+	except Exception as e:
+		logging.exception('Error in get_movie_details_from_html()',exc_info=True)
 	return strings[movie_start:]
 
 def get_omdb_movie_details(movie_name):
-    URL = 'http://www.omdbapi.com/'
-    details = {'plot' : ''
-               ,'id' : ''
-               ,'year' : ''}
-    dir = os.path.dirname(__file__)
-    fn = os.path.join(dir, 'configs/omdbapi.json')
-    with open(fn) as data_file:
-        apikey = json.load(data_file)['apikey']
-
-    params = { 'apikey' : apikey
-               ,'plot' : 'full'
-               ,'r' : 'json'
-               ,'type' : 'movie'
-               ,'s' : movie_name
-               ,'page' : 1
-            }
-    target_movie = {'movie_name' : 'movie'
-                    ,'id' : '0'
-                    ,'year' : '1901'}
-    #populate possible movies
-    movies = []
-    while True:
-        r = requests.get(URL, params = params)
-        results = r.json()
-        if results['Response'] == 'False':
-            break     
-        for movie in results['Search']:
-			movies.append(movie)
-        params['page'] += 1
-    #first look for match with exact movie title and a poster
-    for m in movies:
-        if m['Poster'] != 'N/A' and m['Title'].upper() == movie_name.upper() and (int(m['Year']) > int(target_movie['year'])) and (int(m['Year']) <= int(d.now().strftime('%Y'))):
-            target_movie['movie_name'] = m['Title']
-            target_movie['id'] = m['imdbID']
-            target_movie['year'] = m['Year']
-    #if nothing found (id still = 0) now look for the most recent matching movie
-    if target_movie['id'] == '0':
-        for m in movies:
-            if (int(m['Year']) > int(target_movie['year'])) and (int(m['Year']) <= int(d.now().strftime('%Y'))):
-                target_movie['movie_name'] = m['Title']
-                target_movie['id'] = m['imdbID']
-                target_movie['year'] = m['Year']
-    #now we have (or not) our match and let's continue
-    if target_movie['id'] != '0' and target_movie['year'] != '1901':
-        params['s'] = ''
-        params['i'] = target_movie['id']
-        params['y'] = target_movie['year']
-        s = requests.get(URL, params = params)
-        details['plot'] = s.json()['Plot']
-        details['id'] = s.json()['imdbID']
-        details['year'] = s.json()['Year']
-        logger.info('Got OMDB movie {}, Year {}, ID {}'.format(movie_name,details['year'], details['id']))
-    return details
-    
-def split_strings_into_movies(strings,date):
-	temp = {}
-	for idx, a in enumerate(strings):
-		if 'Running Time' in a:
-			temp[idx] = 'run_time'
-		elif a.upper() == a:
-			temp[idx] = 'movie_title'
+	try:
+		URL = 'http://www.omdbapi.com/'
+		details = {'plot' : ''
+				   ,'id' : ''
+				   ,'year' : ''}
+		dir = os.path.dirname(__file__)
+		fn = os.path.join(dir, 'configs/omdbapi.json')
+		with open(fn) as data_file:
+			apikey = json.load(data_file)['apikey']
+		
+		params = { 'apikey' : apikey
+				   ,'plot' : 'full'
+				   ,'r' : 'json'
+				   ,'type' : 'movie'
+				   ,'s' : movie_name
+				   ,'page' : 1
+				}
+		target_movie = {'movie_name' : 'movie'
+						,'id' : '0'
+						,'year' : '1901'}
+		#populate possible movies
+		movies = []
+		while True:
+			r = requests.get(URL, params = params)
+			results = r.json()
+			if results['Response'] == 'False':
+				break     
+			for movie in results['Search']:
+				movies.append(movie)
+			params['page'] += 1
+		#first look for match with exact movie title and a poster
+		for m in movies:
+			if m['Poster'] != 'N/A' and m['Title'].upper() == movie_name.upper() and (int(m['Year']) > int(target_movie['year'])) and (int(m['Year']) <= int(d.now().strftime('%Y'))):
+				target_movie['movie_name'] = m['Title']
+				target_movie['id'] = m['imdbID']
+				target_movie['year'] = m['Year']
+		#if nothing found (id still = 0) now look for the most recent matching movie
+		if target_movie['id'] == '0':
+			for m in movies:
+				if (int(m['Year']) > int(target_movie['year'])) and (int(m['Year']) <= int(d.now().strftime('%Y'))):
+					target_movie['movie_name'] = m['Title']
+					target_movie['id'] = m['imdbID']
+					target_movie['year'] = m['Year']
+		#now we have (or not) our match and let's continue
+		if target_movie['id'] != '0' and target_movie['year'] != '1901':
+			params['s'] = ''
+			params['i'] = target_movie['id']
+			params['y'] = target_movie['year']
+			s = requests.get(URL, params = params)
+			details['plot'] = s.json()['Plot']
+			details['id'] = s.json()['imdbID']
+			details['year'] = s.json()['Year']
+		if details['id'] == '':
+			logger.info('Didn''t find OMDB movie for {}'.format(movie_name))
 		else:
-			temp[idx] = 'show_time'
-	movie_starts = []
-	for t,value in temp.items():
-		if value == 'movie_title':
-			movie_starts.append(t)
-	movies = []
-	for idx,a in enumerate(strings):
-		if idx in movie_starts:
-			run_time_counter = 0
-			movie = {}
-			movie['name'] = strings[idx + 0]
-			movie['run_time'] = get_run_time(strings[idx + 1])
-			movie['date'] = d.strftime(date,'%Y-%m-%d')
-			movie_details = get_omdb_movie_details(movie['name'])
-			movie['plot'] = movie_details['plot']
-			movie['id'] = movie_details['id']
-			movie['year'] = movie_details['year']
-			for idx2, a2 in enumerate(strings[idx+2:],idx+2):
-				if idx2 in movie_starts:
-					break
-				else:
-					the_str = strings[idx + 2 + run_time_counter]
-					the_str = the_str.replace('(','').replace(')','').replace('Sold Out','').strip()
-					movie['show_time_{}'.format(run_time_counter)] = the_str
-					run_time_counter += 1
-			movie = split_show_times(movie)
-			movies += movie
+			logger.info('Got OMDB movie {}, Year {}, ID {}'.format(movie_name,details['year'], details['id']))
+	except Exception as e:
+		logging.exception('Error in get_omdb_movie_details({})'.format(movie_name),exc_info=True)
+	return details
+	
+def split_strings_into_movies(strings,date):
+	try:
+		temp = {}
+		for idx, a in enumerate(strings):
+			if 'Running Time' in a:
+				temp[idx] = 'run_time'
+			elif a.upper() == a:
+				temp[idx] = 'movie_title'
+			else:
+				temp[idx] = 'show_time'
+		movie_starts = []
+		for t,value in temp.items():
+			if value == 'movie_title':
+				movie_starts.append(t)
+		movies = []
+		for idx,a in enumerate(strings):
+			if idx in movie_starts:
+				run_time_counter = 0
+				movie = {}
+				movie['name'] = strings[idx + 0]
+				movie['run_time'] = get_run_time(strings[idx + 1])
+				movie['date'] = d.strftime(date,'%Y-%m-%d')
+				movie_details = get_omdb_movie_details(movie['name'])
+				movie['plot'] = movie_details['plot']
+				movie['id'] = movie_details['id']
+				movie['year'] = movie_details['year']
+				for idx2, a2 in enumerate(strings[idx+2:],idx+2):
+					if idx2 in movie_starts:
+						break
+					else:
+						the_str = strings[idx + 2 + run_time_counter]
+						the_str = the_str.replace('(','').replace(')','').replace('Sold Out','').strip()
+						movie['show_time_{}'.format(run_time_counter)] = the_str
+						run_time_counter += 1
+				movie = split_show_times(movie)
+				movies += movie
+	except Exception as e:
+		logging.exception('Error in split_strings_into_movies({})'.format(date),exc_info=True)
 	return movies
 
 def split_show_times(movie):
-	temp_movies = []
-	for key,value in movie.items():
-		temp_movie = {}
-		temp_movie['name'] = movie['name']
-		temp_movie['run_time'] = movie['run_time']
-		temp_movie['date'] = movie['date']
-		temp_movie['plot'] = movie['plot']
-		temp_movie['id'] = movie['id']
-		temp_movie['year'] = movie['year']
-		if 'show_time' in key:
-			temp_movie['show_time'] = d.strftime(parser.parse(value),'%H:%M')
-			temp_movies.append(temp_movie)
+	try:
+		temp_movies = []
+		for key,value in movie.items():
+			temp_movie = {}
+			temp_movie['name'] = movie['name']
+			temp_movie['run_time'] = movie['run_time']
+			temp_movie['date'] = movie['date']
+			temp_movie['plot'] = movie['plot']
+			temp_movie['id'] = movie['id']
+			temp_movie['year'] = movie['year']
+			if 'show_time' in key:
+				temp_movie['show_time'] = d.strftime(parser.parse(value),'%H:%M')
+				temp_movies.append(temp_movie)
+	except Exception as e:
+		logging.exception('Error in split_show_times({})'.format(movie['name']),exc_info=True)
 	return temp_movies
 	
 
 def get_run_time(str_run_time):
-	split_str = str_run_time.split(' ')
-	for index,s in enumerate(split_str):
-		try:
-			hr = int(s)
-			mn = int(split_str[index + 2])
-			run_time = '{}:{}'.format(hr,'00{}'.format(mn)[-2:])
-			return run_time
-			break
-		except Exception as ex:
-			pass
-    
+	try:
+		split_str = str_run_time.split(' ')
+		for index,s in enumerate(split_str):
+			try:
+				hr = int(s)
+				mn = int(split_str[index + 2])
+				run_time = '{}:{}'.format(hr,'00{}'.format(mn)[-2:])
+				return run_time
+				break
+			except Exception as ex:
+				pass
+	except Exception as e:
+		logging.exception('Error in get_run_time({})'.format(str_run_time),exc_info=True)
+	
 def main():
 	movies = []
 	for date in get_dates():
 		strings = get_daily_html(date)
 		strings = get_movie_details_from_html(strings)
-		daily_movies = split_strings_into_movies(strings,date)			
+		daily_movies = split_strings_into_movies(strings,date)            
 		movies += daily_movies
 	return movies
 
 	
 	
 if __name__ == '__main__':
-        m = main()
-        print(json.dumps(m,indent=4))
+	m = main()
+	print(json.dumps(m,indent=4))
