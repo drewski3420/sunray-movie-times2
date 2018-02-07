98c1289e019fc6667a7678ec10cf9b3c85a9fd09
Drew Jowers <drewski3420@gmail.com>
2018-02-07 18:36:05 -0500

    changed the log directory again

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ def setup_custom_logger(name):
     #logging.getLogger('chardet').setLevel(logging.WARNING)
     
     dir = os.path.dirname(__file__)
-    fn = os.path.join(dir, '/var/log/sunray.log')
+    fn = os.path.join(dir, '~/sunray.log')
     formatter = logging.Formatter(fmt='%(asctime)s | %(name)s | %(levelname)-8s | %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
     handler = logging.FileHandler(fn, mode='a',encoding = 'UTF-8')
